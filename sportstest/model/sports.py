8c32654141055c5203c511d8015600bacdbd0674
aruymeek <68474848+aruymeek@users.noreply.github.com>
2020-08-10 15:48:42 +0900

    Add files via upload

---
+++ sportstest/model/sports.py
@@ -0,0 +1,9 @@
+class SportsModel:
+
+    def __init__(self, _title, _class1, _href, _imgsrc, _content):
+        self.title = _title
+        self.class1 = _class1
+        self.href = _href
+        self.imgsrc = _imgsrc
+        self.content = _content
+
